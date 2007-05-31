d5bbbbd6385aad36d0cd4709efe10b71431d157f
Bernard Cafarelli <voyageur@gentoo.org>
2007-05-31 15:21:45 +0000

    Moved aclock to System (with simpler convenience script)
    svn path=/overlay/; revision=67

---
+++ gnustep-apps/aclock/files/config-aclock.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "defaults write AClock SmoothSeconds YES"
+defaults write AClock SmoothSeconds YES
+echo "defaults write AClock RefreshRate 0.1"
+defaults write AClock RefreshRate 0.1
