33225c4ae3d54785c781ba6bf5dfb16854263fc9
Bernard Cafarelli <voyageur@gentoo.org>
2007-06-04 14:42:04 +0000

    Simpler config script (hardcode paths)
    svn path=/overlay/; revision=87

--- gnustep-libs/camaelon/files/config-camaelon.sh
+++ gnustep-libs/camaelon/files/config-camaelon.sh
@@ -1,5 +1,5 @@
 echo "Applying Camaelon default theme..."
-echo "defaults write NSGlobalDomain GSAppKitUserBundles \"(${GNUSTEP_SYSTEM_LIBRARY}/Bundles/Camaelon.themeEngine)\""
-defaults write NSGlobalDomain GSAppKitUserBundles "(${GNUSTEP_SYSTEM_LIBRARY}/Bundles/Camaelon.themeEngine)"
+echo "defaults write NSGlobalDomain GSAppKitUserBundles \"(/usr/GNUstep/System/Library/Bundles/Camaelon.themeEngine)\""
+defaults write NSGlobalDomain GSAppKitUserBundles "(/usr/GNUstep/System/Library/Bundles/Camaelon.themeEngine)"
 echo "defaults write Camaelon Theme Nesedah"
 defaults write Camaelon Theme Nesedah
