0edf55728a268af1135f3a7e3277930912bc2853
Bernard Cafarelli <voyageur@gentoo.org>
2007-06-20 20:57:52 +0000

    Prevent gnustep profile.d file from running in non-interactive shells
    svn path=/overlay/; revision=115

--- gnustep-base/gnustep-make/files/gnustep-2.sh
+++ gnustep-base/gnustep-make/files/gnustep-2.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Test for an interactive shell
+if [[ $- != *i* ]]; then
+	return
+fi
+	
 GNUSTEP_SYSTEM_TOOLS=/usr/GNUstep/System/Tools
 
 if [ -x ${GNUSTEP_SYSTEM_TOOLS}/make_services ]; then
